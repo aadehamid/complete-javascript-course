4a2c5927a553814f289816767676f0326df5aeb1
Hamid Adesokan <aadehamid@gmail.com>
2023-04-16 09:07:00 -0500

    Started a new JS playground

---
+++ JSplayground/01-Fundamentals/assignment1.js
@@ -0,0 +1,4 @@
+let country = "USA";
+let continent = "North America";
+let population = 330000000;
+console.log(`The population of ${country} is ${population} and it is located in ${continent}.`);
---
+++ JSplayground/01-Fundamentals/part1.js
@@ -0,0 +1,3 @@
+let js = "Amazing";
+if (js === "Amazing") alert("JavaScript is FUN!");
+console.log(40 + 10 + 10 + 10);
